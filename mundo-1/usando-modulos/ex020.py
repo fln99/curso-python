a76e6730c325336b0040f6f514fae8b99f053bf1
Felipe Nunes <felipeln.99@gmail.com>
2020-06-01 15:52:33 -0300

    update in an exercise

--- mundo-1/usando-modulos/ex020.py
+++ mundo-1/usando-modulos/ex020.py
@@ -1,9 +1,11 @@
-from random import sample
+from random import shuffle
 
 aluno1 = input('Primeiro aluno: ')
 aluno2 = input('Segundo aluno: ')
 aluno3 = input('Terceiro aluno: ')
 aluno4 = input('Quarto aluno: ')
 
+lista = [aluno1, aluno2, aluno3, aluno4]
+shuffle(lista)
 print('A ordem de apresentação resultou em', end=' >>> ')
-print('{}!'.format(sample([aluno1, aluno2, aluno3, aluno4], 4)))
+print('{}!'.format(lista))
