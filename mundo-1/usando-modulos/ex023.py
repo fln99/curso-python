91c0e6f7b3ffd8a8aca1992d552bad5a9776c08f
Felipe Nunes <felipeln.99@gmail.com>
2020-06-02 14:28:08 -0300

    update ex024

--- mundo-1/usando-modulos/ex023.py
+++ mundo-1/usando-modulos/ex023.py
@@ -1,9 +1,11 @@
 numero = int(input('Insira um número de 0 a 9999: '))
 
-u = str(numero % 10 / 1000).replace('.', '')
-d = str(numero % 100 / 1000).replace('.', '')
-c = str(numero % 1000 / 1000).replace('.', '')
-m = str(numero % 10000 / 1000).replace('.', '')
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
 
-print('Sobre o número inserido: ')
-print('Milhar: {} \nCentena: {} \nDezena: {} \nUnidade: {}'.format(m, c, d, u))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
