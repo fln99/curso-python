dda854104d932221a158d8498d7e40c24e54f00e
Felipe Nunes <felipeln.99@gmail.com>
2020-06-09 08:54:35 -0300

    update of 42nd exercise

--- mundo-2/condicoes/ex042.py
+++ mundo-2/condicoes/ex042.py
@@ -3,7 +3,11 @@ lado_b = int(input('Aresta B do triângulo: '))
 lado_c = int(input('Aresta C do triângulo: '))
 
 if lado_a < lado_b + lado_c and lado_b < lado_a + lado_c and lado_c < lado_a + lado_b:
-    if lado_a == lado_b or lado_b == lado_a or lado_c == lado_a:
-        print('O triângulo formado é do tipo Isósceles.')
-    elif lado_a == lado_b and lado_a == lado_c and lado_b == lado_a and
+    if lado_a == lado_b and lado_b == lado_c:
+        print('Seu triângulo é Equilátero')
+    elif lado_a == lado_b or lado_b == lado_c:
+        print('Seu triângulo é Isósceles')
+    else:
+        print('O triângulo formado é Escaleno!')
 else:
+    print('Não é possível formar triângulo com as medidas informadas!')
