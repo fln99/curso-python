d68ee7d5221bb3eabdc75443101725c20dfb9f35
Felipe Nunes <felipeln.99@gmail.com>
2020-06-09 16:57:47 -0300

    new

--- mundo-2/repeticoes-for/ex055.py
+++ mundo-2/repeticoes-for/ex055.py
@@ -1,14 +1,12 @@
-
 maior_peso = 0
 menor_peso = 0
 controle = 0
 
 for i in range(0, 5):
     peso = float(input('Insira seu peso aqui: '))
-    
+
     if controle == 0:
         controle = peso
-
     if peso > controle:
         maior_peso = peso
     else:
