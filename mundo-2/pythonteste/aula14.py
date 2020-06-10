c778cddeaef593f00aca2c19ca80e795e9996f5c
Felipe Nunes <felipeln.99@gmail.com>
2020-06-10 10:18:54 -0300

    Create aula14.py

---
+++ mundo-2/pythonteste/aula14.py
@@ -0,0 +1,14 @@
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um número: '))
+    r = str(input('Quer continuar? [S/N] '))
+
+# for c in range(1, 5):
+#     n = int(input('Digite um valor: '))
+# print('End')
+
+# c = 1
+# while c < 11:
+#     print(c)
+#     c += 1
+# print('End')
