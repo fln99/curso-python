bc8c3955d4ae1a913c72a981c238b5203a3cd633
Felipe Nunes <felipeln.99@gmail.com>
2020-06-14 12:41:37 -0300

    Update ex070.py

--- mundo-2/while-break/ex070.py
+++ mundo-2/while-break/ex070.py
@@ -2,28 +2,29 @@ print('-' * 19)
 print('{:^20}'.format('Python Magazine'))
 print('-' * 19)
 
-total = mil = menor = 0
+total_compra = mais_mil = 0
 
-nome_p = ''
+ultimo_produto = {}
+
+mais_barato = {}
 
 while True:
-    nome = str(input('Qual o nome do produto? ')).strip()
-    preco = float(input('Preço do produto: '))
-    continuar = str(input('Deseja continuar comprando? [S/N] ')).strip().upper()
+    nome_produto = str(input('Nome do produto: ')).strip()
+    preco_produto = float(input('Preço do produto: '))
+    continuacao = str(input('Deseja continuar comprando? [S/N] ')).strip().upper()[0]
 
-    total += preco
+    total_compra += preco_produto
+    
+    ultimo_produto = {'nome':nome_produto, 'preco':preco_produto}
 
-    if preco > 1000:
-        mil += 1
+    if preco_produto < ultimo_produto['preco']:
+            mais_barato = {'nome':nome_produto, 'preco':preco_produto}
 
-    menor = preco
+    if preco_produto > 1000:
+            mais_mil += 1
 
-    if preco < menor:
-        menor = preco
-        nome_p = nome
-
-    if continuar in 'Nn':
+    if continuacao == 'N':
+        print(f'O total gasto na compra foi de R${total_compra}!')
+        print(f'Há um total de {mais_mil} produtos de mais de R$1000 reais.')
+        print(f'o produto mais barato foi {mais_barato}!')
         break
-
-print(f'O total da compra é R${total:.2f}. Existem {mil} produtos que custam mais de R$1000')
-print(f'O produto mais barato é {nome_p} e custa R${menor}!')
