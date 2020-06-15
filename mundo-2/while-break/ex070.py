ffebfdf8152ff69b84b6c901eb552c2df73bc4d3
Felipe Nunes <felipeln.99@gmail.com>
2020-06-15 07:47:40 -0300

    update two exercises

--- mundo-2/while-break/ex070.py
+++ mundo-2/while-break/ex070.py
@@ -2,29 +2,26 @@ print('-' * 19)
 print('{:^20}'.format('Python Magazine'))
 print('-' * 19)
 
-total_compra = mais_mil = 0
+total_compra = maiores_mil = 0
 
-ultimo_produto = {}
-
-mais_barato = {}
+mais_barato = 99999999
 
 while True:
-    nome_produto = str(input('Nome do produto: ')).strip()
+    nome_produto = str(input('Nome do produto: ')).strip().capitalize()
     preco_produto = float(input('Preço do produto: '))
-    continuacao = str(input('Deseja continuar comprando? [S/N] ')).strip().upper()[0]
+    escolha = str(input('Você quer continuar comprando? [S/N] ')).strip().upper()[0]
 
     total_compra += preco_produto
-    
-    ultimo_produto = {'nome':nome_produto, 'preco':preco_produto}
-
-    if preco_produto < ultimo_produto['preco']:
-            mais_barato = {'nome':nome_produto, 'preco':preco_produto}
 
     if preco_produto > 1000:
-            mais_mil += 1
+        maiores_mil += 1
+    
+    if preco_produto < mais_barato:
+        mais_barato = preco_produto
+        produto = nome_produto
 
-    if continuacao == 'N':
-        print(f'O total gasto na compra foi de R${total_compra}!')
-        print(f'Há um total de {mais_mil} produtos de mais de R$1000 reais.')
-        print(f'o produto mais barato foi {mais_barato}!')
+    if escolha == 'N':
+        print(f'O total da compra: R${total_compra:.2f}')
+        print(f'Sua compra contém {maiores_mil} produtos acima de R$1000 reais')
+        print(f'O produto mais barato da sua cesta é {produto} e custa R${mais_barato:.2f}!')
         break
---
+++ mundo-2/while-break/ex071.py
@@ -0,0 +1,10 @@
+print('-' * 30)
+print('{:^30}'.format('$ PythonBank $'))
+print('{:^30}'.format('Saque teu dinheiro!'))
+print('-' * 30)
+
+print('Insira um valor inteiro!\nCaixa eletrônico não tem moeda.')
+print('-' * 30)
+
+valor_a_sacar = int(input('Qual o valor a ser sacado? '))
+
