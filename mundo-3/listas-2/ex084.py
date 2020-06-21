4c293f65702d92678e2e4b7a6f7eb1b363946220
Felipe Nunes <felipeln.99@gmail.com>
2020-06-21 12:36:12 -0300

    first exercises of lists part 2

---
+++ mundo-3/listas-2/ex084.py
@@ -0,0 +1,35 @@
+pessoas = list()
+pessoa = list()
+
+while True:
+    pessoa.append(str(input('Informe seu nome: ')).strip().capitalize())
+    pessoa.append(float(input('Informe seu peso: ')))
+
+    pessoas.append(pessoa[:])
+    pessoa.clear()
+
+    continuar = str(input('Deseja continuar? [S/N] '))
+    if continuar in 'Nn':
+        break
+
+maior = menor = 0
+maior_peso = list()
+menor_peso = list()
+
+for c in pessoas:
+    if c == 0:
+        maior = menor = 0
+    if c[1] > maior:
+        maior = c[1]
+    else:
+        menor = c[1]
+
+for c in pessoas:
+    if c[1] == maior:
+        maior_peso.append(c[0])
+    else:
+        menor_peso.append(c[0])
+
+print(f'Foram cadastradas {len(pessoas)} pessoas na lista!')
+print(f'As pessoas com {maior} são: {maior_peso}')
+print(f'As pessoas com {menor} são: {menor_peso}')
