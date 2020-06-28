0547b8cd9b1f5885c27efac6783f8f611ad082ac
Felipe Nunes <felipeln.99@gmail.com>
2020-06-28 19:27:43 -0300

    new module

---
+++ mundo-3/modularizacao/uteis/numeros/__init__.py
@@ -0,0 +1,13 @@
+def fatorial(num):
+    fator = 1
+    for c in range(num, 1, -1):
+        fator *= c
+    return fator
+
+
+def dobro(num):
+    return num * 2
+
+
+def triplo(num):
+    return num * 3
