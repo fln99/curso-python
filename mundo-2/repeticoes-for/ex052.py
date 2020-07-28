23176a65cec6d2e1e114ac7ff500d6b0dac95630
Felipe Nunes <59563220+fln99@users.noreply.github.com>
2020-07-28 10:56:25 -0300

    update conditional logic

--- mundo-2/repeticoes-for/ex052.py
+++ mundo-2/repeticoes-for/ex052.py
@@ -1,6 +1,6 @@
 num = int(input('Insira um número inteiro: '))
 
-if num % num == 0 and num % 1 == 0 and num % 2 != 0:
+if numero // numero == 1 and numero // 1 == numero and numero % 2 != 0 or numero == 2:
     print('É um número primo!')
 else:
     print('Não é número primo.')
